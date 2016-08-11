399e13607c11b3dfdd07cfe04bb2540f9086796a
mayotunde <fritzmaver@rocketmail.com>
2016-08-11 15:59:34 +0100

    Add files via upload

---
+++ json_Cluster1.js
@@ -0,0 +1,15 @@
+var json_Cluster1={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "0", "count": "13", "scale": "10", "distance": 0.076437028285175998 }, "geometry": { "type": "Point", "coordinates": [ 3.975555628182047, 7.469654869907139 ] } },
+{ "type": "Feature", "properties": { "id": "4", "count": "11", "scale": "10", "distance": 0.076437028285175998 }, "geometry": { "type": "Point", "coordinates": [ 3.817266373326339, 7.421326120949232 ] } },
+{ "type": "Feature", "properties": { "id": "7", "count": "20", "scale": "10", "distance": 0.076437028285175998 }, "geometry": { "type": "Point", "coordinates": [ 3.947909252000429, 7.283394217654072 ] } },
+{ "type": "Feature", "properties": { "id": "5", "count": "3", "scale": "10", "distance": 0.076437028285175998 }, "geometry": { "type": "Point", "coordinates": [ 3.767550866679206, 7.472988528797619 ] } },
+{ "type": "Feature", "properties": { "id": "6", "count": "8", "scale": "10", "distance": 0.076437028285175998 }, "geometry": { "type": "Point", "coordinates": [ 3.761834449525281, 7.354004532595653 ] } },
+{ "type": "Feature", "properties": { "id": "2", "count": "21", "scale": "10", "distance": 0.076437028285175998 }, "geometry": { "type": "Point", "coordinates": [ 3.866023931216638, 7.346243442323904 ] } },
+{ "type": "Feature", "properties": { "id": "8", "count": "9", "scale": "10", "distance": 0.076437028285175998 }, "geometry": { "type": "Point", "coordinates": [ 3.824720114042078, 7.263529715723855 ] } },
+{ "type": "Feature", "properties": { "id": "1", "count": "59", "scale": "10", "distance": 0.076437028285175998 }, "geometry": { "type": "Point", "coordinates": [ 3.926597357114592, 7.39126246058227 ] } },
+{ "type": "Feature", "properties": { "id": "3", "count": "10", "scale": "10", "distance": 0.076437028285175998 }, "geometry": { "type": "Point", "coordinates": [ 3.887417238964457, 7.476799534151223 ] } }
+]
+}
